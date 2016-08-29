0e15e7b4ea4919e7f8ca543420aeb8a91dc6db9c
Denis <burdden@gmail.com>
2016-08-29 15:00:59 +0300

    update controllerjs

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -2,14 +2,14 @@ var	cartApp	=	angular.module('cartApp',	[]);
 cartApp.controller('cartCtrl',		function	($scope,	$http)	{
 		
 		$scope.refreshCart	=	function(cartId)	{
-				$http.get('/shop/rest/cart/'+$scope.cartId)
+				$http.get('/rest/cart/'+$scope.cartId)
 				.success(function(data)	{
 						$scope.cart	=	data;
 				});
 		};
 		
 		$scope.clearCart	=	function()	{
-				$http.delete('/shop/rest/cart/'+$scope.cartId)
+				$http.delete('/rest/cart/'+$scope.cartId)
 				.success($scope.refreshCart($scope.cartId));
 				
 		};
@@ -20,16 +20,16 @@ cartApp.controller('cartCtrl',		function	($scope,	$http)	{
 		};
 		
 		$scope.addToCart	=	function(productId)	{
-				$http.put('/shop/rest/cart/add/'+productId)
+				$http.put('/rest/cart/add/'+productId)
 				.success(function(data)	{
-						$scope.refreshCart($http.get('/shop/rest/cart/get/cartId'));
+						$scope.refreshCart($http.get('/rest/cart/get/cartId'));
 						alert("Товар успешно добавлен в корзину");
 				});
 		};
 		$scope.removeFromCart	=	function(productId)	{
-				$http.put('/shop/rest/cart/remove/'+productId)
+				$http.put('/rest/cart/remove/'+productId)
 				.success(function(data)	{
-						$scope.refreshCart($http.get('/shop/rest/cart/get/cartId'));
+						$scope.refreshCart($http.get('/rest/cart/get/cartId'));
 				});
 		};
 });
